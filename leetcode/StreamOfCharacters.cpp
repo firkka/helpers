a7e2914c5d3725fd2432a6b5b83205c1e0b522bf
Jukka Peltola <firkka@gmail.com>
2020-08-24 08:45:42 +0300

    Create StreamOfCharacters.cpp

---
+++ leetcode/StreamOfCharacters.cpp
@@ -0,0 +1,30 @@
+/*
+Implement the StreamChecker class as follows:
+
+    StreamChecker(words): Constructor, init the data structure with the given words.
+    query(letter): returns true if and only if for some k >= 1, the last k characters
+    queried (in order from oldest to newest, including this letter just queried) spell one of the words in the given list.
+
+*/
+
+class StreamChecker
+{
+private:
+    vector<string> words;
+public:
+    StreamChecker(vector<string>& words) : words(words)
+    {
+        
+    }
+    
+    bool query(char letter)
+    {
+        return false;
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
